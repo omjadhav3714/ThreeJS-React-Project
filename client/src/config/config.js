ee480a351a6a973dff7fb159b0ab9dfdbbb3549b
omjadhav3714 <omjadhav963@gmail.com>
2023-05-28 21:06:00 +0530

    backend url updated

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,9 @@
 const config = {
   development: {
-    backendUrl: "http://localhost:8080/api/v1/dalle",
+    backendUrl: "https://threejs-react-gs03.onrender.com",
   },
   production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    backendUrl: "https://threejs-react-gs03.onrender.com",
   },
 };
 
